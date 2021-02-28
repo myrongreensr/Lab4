fa93ee47d9accdfc391d59db326cd65cf7db1b60
myrongreensr <myrongreensr2002@gmail.com>
2021-02-28 16:12:09 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Myron Green Test Web Design </title>
+<meta charset="utf-8">
+</head>
+
+<body>
+<header>
+<body style="background-color:#F5F5F5;color:#008080;">
+<h1 style="background-color:#008080;color:#F5F5F5;">Myron Green Test Media Design Website</h1>
+</header>
+<nav>
+  <b><a href="index.php">Home</a> &nbsp; 
+     <a href="services.php">Services</a> &nbsp; 
+	 <a href="contact.php">Contact</a>
+  </b>
+</nav>
+<main>
+ <h2>New Media and Web Design</h2>
+ <p>Trillium Media Design offers a comprehensive range of services to take your company&#39;s Web presence to the next level. </p>
+ <h2>Meeting Your Business Needs</h2>
+ <p>Our expert designers will listen to you as they create a website that helps to promote and grow your business.</p>
+
+ <h2>Questions? Email Webmaster</h2>
+ <li>E-mail: <a href="mailto:myrongreensr2002@gmail.com">myrongreensr2002@gmail.com</a></li>
+
+ <p>I'm currently a student trying to complete my Masters in Computer information Technology </p> 
+ <p>I've been studying at Dallas Baptist University located in Dallas, Texas for the past two years. <a href="https://www.dbu.edu/">dbu.edu</a></p> 
+ 
+
+
+ <h1>Student: Myron Green </h1>
+  <map id="Myron Green" name="Myron Green">
+  <li>School Website: <a href="https://www.dbu.edu/">Dallas Baptist University</a></li>
+      <area href="https://www.dbu.edu/"  shape="rect" coords="0,51,416,170" alt="Dallas Baptist University" title="Dallas Baptist Univesrity">
+      <area href="https://www.dbu.edu/"  shape="rect" coords="24,188,339,283" alt="Dallas Baptist University" title="Dallas Baptist University">
+   </map>
+   <img src="Junior Achievement 2017.png" usemap="#fishing" alt="Door County" height="150" width="300">
+
+ 
+</main>
+<footer>
+  <small><i>Copyright &copy; 2018 Your Name Here</i></small>
+</footer>
+</body>
+</html>
+
+
+
+
+
